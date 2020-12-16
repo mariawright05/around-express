605647a95eb3b160a226fcaa7505e1f50c288fc2
Maria Wright <maria@mariawrightstudios.com>
2020-12-16 11:17:33 -0500

    minor changes

--- workspace/server/index.js
+++ workspace/server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex:true,
+  useFindAndModify: false
+});
 
 const userRouter = require('./routers/users');
 const cardRouter = require('./routers/cards');
